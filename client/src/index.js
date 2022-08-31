6edb50672a356b5c4314e91da180062ff70df5fa
Aditya <adityaaryan.m4@gmail.com>
2022-08-31 13:36:30 +0530

    26th  commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import styles from "./styles.css"
 import { Provider } from "react-redux";
 import store, { persistor } from "./store/store";
 import { PersistGate } from 'redux-persist/integration/react'
